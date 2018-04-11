58a32b068c6d8879305e5d9c8c14a0a6d47c114e
KenHayChung <khchu14@student.monash.edu>
2018-04-11 13:52:01 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//initialise external modules for server, websocket, arduino and create instances of them
 var express = require("express");
 var app = express();
 var fs = require("fs");
@@ -7,6 +8,8 @@ var io = require("socket.io")(server);
 var five = require("johnny-five");
 var board = new five.Board();
 var admin = require("firebase-admin");
+
+//initialise global variables
 var sensor = false;
 var motion_length = 0;
 var counter = 0;
@@ -17,15 +20,18 @@ var light_switch = false;
 
 
 
-
+//set home page of the server to the local file 'index.html'
 app.get('/', function(req, res, next){
 	res.sendFile(path + '/index.html');
 });
 
+//location of the server
 server.listen(5000, function(){
 	console.log("Listening on port 5000!");
 });
 
+
+//initialising the webApp and linking it to the correct firebase database
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -34,7 +40,7 @@ admin.initializeApp({
 });
 
 
-
+//function to push the detected motion & its type into the database (whether it's a 'person' or not isn't decided yet)
 function push_motion(){
 	if(motion_length > 8000){
 			ref.push({
@@ -51,7 +57,8 @@ function push_motion(){
 				classification: null
 			});
 		}	
-	recount();
+	recount(); //update the totals on the webpage as something should have just been added
+	//get last 4 entries into the database
 	var motionRef = admin.database().ref("motions").limitToLast(4);	
 	sequence = [];
 	counter = 0;	
@@ -60,14 +67,15 @@ function push_motion(){
 
 function show_msg(data){
 	var msg = data.val();
-	sequence.push(msg.type);
+	sequence.push(msg.type); //get the motion types of the last 4 entries (including itself)
 	counter ++;
 	if (counter == 4){
 		find_person(sequence);
 	}
 }
 
-function find_person(x){
+//function that calls 'is_person' (self explanatory) and calls 'convert_thing' which converts the boolean from 'is_person' into a string
+function find_person(x){	
 	if (x.length == 4){	
 		var thing = is_person(x);
 		var person = convert_thing(thing);
@@ -75,7 +83,7 @@ function find_person(x){
 	}
 }
 
-
+//function that writes 'person' if they are considered a person and 'other' if not
 function convert_thing(x){
 	if (x == true){
 		return ('person');
@@ -84,6 +92,7 @@ function convert_thing(x){
 	};
 }
 
+//function that checks if the entry is a 'person' (the last 4 motion types are 'long', 'short', 'long', 'long')
 function is_person(array){
 	if (array[0] == 'long' && array[2] == 'long' && array[3] == 'long'){
 		if (array[1] == 'short'){
@@ -97,11 +106,11 @@ function is_person(array){
 }
 
 
-
+//initialise database references
 var db = admin.database();
 var ref = db.ref("motions");
 
-
+//counts the number of 'long' motions in the database
 function count_long(){
 	var count = 0;
 	var ref = admin.database().ref("motions");
@@ -111,6 +120,7 @@ function count_long(){
 	return count;
 }
 
+//counts the number of 'short' motions in the database
 function count_short(){
 	var count = 0;
 	var ref = admin.database().ref("motions");
@@ -120,6 +130,7 @@ function count_short(){
 	return count;
 }
 
+//counts the number of 'people' in the database
 function count_people(){
 	var count = 0;
 	var pRef = admin.database().ref("motions");
@@ -130,7 +141,7 @@ function count_people(){
 }
 
 
-
+//function that calls all the counting functions to update the webpage
 function recount(){
 	var long = count_long();
 	var short = count_short();
@@ -139,18 +150,23 @@ function recount(){
 	io.emit('return_count', total);
 }
 
+//emit that tells the server that a person has been detected and to turn on the light
 function somebody(){
 	io.emit('somebody');
 }
 
+//initialise and define the arduino board
 board.on("ready", function(){
+	//initiaise the led and the motion sensor	
 	var led = new five.Led(13);
 	var motion = new five.Motion(7);
 	var motion_start;
 	var motion_end;	
+	//tells console when the motion sensor is ready (until this is displayed make sure nothing moves in front of it)
 	motion.on("calibrated", function(){
 			console.log("calibrated");
 		});
+	//when the sensor detects motion save the timestamp
 	motion.on("motionstart", function(){
 	if (sensor == true){		
 		console.log("motionstart");
@@ -158,6 +174,7 @@ board.on("ready", function(){
 		motion_start = d.getTime();
 		}
 	});
+	//when the sensor detects that the motion has stopped, save the timestamp and get the length of the motion by subtracting the latter from the former
 	motion.on("motionend", function(){
 	if (sensor == true){
 		console.log("motionend");
@@ -165,11 +182,13 @@ board.on("ready", function(){
 		motion_end = d.getTime();
 		motion_length = motion_end - motion_start;
 		console.log("motion length = " + motion_length + "ms/ " + motion_length/1000 + "s \n");
+		//put motion into database		
 		push_motion();	
 	}
 	});
-	
+	//websocket definitions
 	io.on("connection", function(client){
+		//when the client pressed the reset button, remove everything and update the webpage
 		client.on('reset', function(){
 			ref.remove();
 			ref.off();	
@@ -180,22 +199,27 @@ board.on("ready", function(){
 			io.emit('return_count', total);
 			console.log('Database Cleared!');
 		});
+		//turning on the sensor
 		client.on('sensor_on', function(){
 			sensor = true;
 			console.log('Sensor On!');
 		});
+		//turning off the sensor
 		client.on('sensor_off', function(){
 			sensor = false;
 			console.log('Sensor Off!');
 		});
+		//turning on the light switch
 		client.on('light_switch_on', function(){
 			light_switch = true;
 			console.log('Light Switch On!');
 		});
+		//turning off the light switch
 		client.on('light_switch_off', function(){
 			light_switch = false;
 			console.log('Light Switch Off!');
 		});
+		//function that counts motions types and people to update on the webpage
 		client.on('count', function(){
 			var long = count_long();
 			var short = count_short();
@@ -203,26 +227,29 @@ board.on("ready", function(){
 			var total = [long, short, no_people];
 			io.emit('return_count', total);
 		});
+		//calls count from the server
 		recount();
+		//calls person_detected from the server
 		somebody();
+		//when a person is detected, turn on the light for 15s, and if another is detected in that period, keep the light on for another 5s
 		client.on('person_detected', function(){						 
-			if (light_switch == true){			
-				if (light == 'off'){
+			if (light_switch == true){			//if the light is enabled from the webpage (off by default)
+				if (light == 'off'){			//if the light is not already on from people recently passing
 					light = 'on';				
-					led.on();
+					led.on();		//turn light on
 					console.log('Person detected - light on for 15 seconds!');
-					setTimeout(
+					setTimeout(				//after 15s, do this
 						function light_off(){
-							led.off();
+							led.off();			//turn light off unless... (see below)
 							light = 'off';
-							if (extra_people != 0){
+							if (extra_people != 0){		//(from above)... an extra person has entered
 								var i = 0;
 								light = 'on';
 								led.on();
 								console.log('Start of extra time!');
-								var interval = setInterval(function(){
+								var interval = setInterval(function(){	//every 5 seconds check if sufficent extra light has been given
 									i++;
-									if (i == extra_people){
+									if (i == extra_people){	//if enough light has been given for the extra people, turn it off
 										extra_people = 0;
 										led.off();
 										light = 'off';
@@ -233,8 +260,8 @@ board.on("ready", function(){
 								}, 5000);
 							}
 						}, 15000);
-				}	else{
-					extra_people += 1;
+				}	else{			//if the light is already on from someone passing recently
+					extra_people += 1;		//log the number of extra people for length of extra illumination time
 					console.log('Another person detected! Light on for additional 5 seconds!');
 				}
 			}
@@ -243,7 +270,7 @@ board.on("ready", function(){
 });
 
 
-
+//function that updates each entry (which right now is just motion type and duration) with their object status and the determining sequence
 function update_person(sequence, thing){
 	var motionRef = admin.database().ref("motions").limitToLast(1);
 	if (thing == 'person'){
